9196d2d7d0f7491b0507aaa7746a069031a50fa2
Yinlin Li <yinlin@seal.io>
2023-08-31 00:13:31 +0800

    Fix main.tf

--- main.tf
+++ main.tf
@@ -11,9 +11,6 @@ data "kubectl_path_documents" "manifest" {
   pattern = "${path.module}/manifest.yaml"
   vars = {
     namespace        = local.namespace
-    image_registry   = var.image_registry
-    image_repository = var.image_repository
-    image_version    = var.image_version
     replicas         = var.replicas
     frontend_name    = var.frontend_name
   }
